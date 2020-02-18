3cdfc46525347c967d7c5e5bc2a1f77a058a205b
Carloshrf <carlos_6282010@hotmail.com>
2020-02-18 15:02:23 -0300

    Mongo connection - notification schema and first register

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import Appointment from '../app/models/Appointment';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   // fará a comunicação com o bd e iniciará os models
@@ -20,6 +22,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
