ba76c6d8a723471c64a30740df89377794401e5b
carloshrf <carlos_6282010@hotmail.com>
2020-02-24 10:39:57 -0300

    exception handling and systems vars

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'e532dec1a2be64',
-    pass: '64a52a524c087d',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
